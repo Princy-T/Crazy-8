62837bcfc05ca0addcf7dc70393f935386c00a40
Princy-T <96741606+Princy-T@users.noreply.github.com>
2022-04-21 18:02:10 +0530

    Update game.java

--- src/main/java/game.java
+++ src/main/java/game.java
@@ -1,9 +1,7 @@
 
-public class game {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Game 
+{
+	public static void main(String[] args)
+	{
 	}
-
 }
