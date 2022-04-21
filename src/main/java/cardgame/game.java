5e9287359bff9771f8a9e4631de6c9abba7e6f18
Princy-T <96741606+Princy-T@users.noreply.github.com>
2022-04-21 18:14:31 +0530

    Update game.java

--- src/main/java/cardgame/game.java
+++ src/main/java/cardgame/game.java
@@ -1,12 +1,12 @@
 package cardgame;
 import java.util.List;
 import java.util.ArrayList;
-public class game 
+public class Game 
 {
 	public static void main(String[] args) 
 	{     
 		HelperFunction gameobj=new HelperFunction();   
-		List<Card> Deck = new ArrayList<>();
+		List<Card> deck = new ArrayList<>();
 		gamer1 play1 = new gamer1();
 		gamer2 play2 = new gamer2();
 		Deck=gameobj.startReStart(Deck,play1,play2);
